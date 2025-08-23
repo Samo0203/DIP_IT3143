d89e1c4e590f7603bce35a29cc41bb24f00faa7f
Samodya <samodya0203samadini@gmail.com>
2025-08-23 15:54:59 +0530

    Add files via upload

---
+++ Day01_08-20/codes/que.js
@@ -0,0 +1,11 @@
+1) swap 2 numbers without temporary variable
+
+2) find  3 large numbers among given array of numbers
+
+3) break a whole number into digits like 4562 -> 4,5,6,2
+
+4) scale a matrix 
+
+5) multiply two matrix
+
+6) create a function to print elements from multi dimensional array
